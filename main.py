fca3e41c6f2fb7f65b0aa49af383f839088967bc
ClausHofman <46158@edu.raseko.fi>
2022-01-31 13:41:35 +0200

    lisätty laatikko

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
 
 from quiz_ui import Ui_MainWindow
 
@@ -28,6 +28,8 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.vaihda_kysymys_ja_vastaukset(0)
         self.kytke_napit()
+        self.pisteet = 0
+        self.indeksi = 0
 
     def vaihda_kysymys_ja_vastaukset(self, indeksi):
         tekstit = KYSYMYKSET_JA_VASTAUKSET[indeksi]
@@ -55,12 +57,36 @@ class MainWindow(QMainWindow):
 
     def kytke_napit(self):
         self.ui.button1.clicked.connect(self.nappia_painettu)
-        self.ui.button1.clicked.connect(self.nappia_painettu)
-        self.ui.button1.clicked.connect(self.nappia_painettu)
-        self.ui.button1.clicked.connect(self.nappia_painettu)
+        self.ui.button2.clicked.connect(self.nappia_painettu)
+        self.ui.button3.clicked.connect(self.nappia_painettu)
+        self.ui.button4.clicked.connect(self.nappia_painettu)
 
     def nappia_painettu(self):
-        print("PAINETTU NAPPIA")
+        if self.sender() == self.ui.button1:
+            nappi = 1
+        elif self.sender() == self.ui.button2:
+            nappi = 2
+        elif self.sender() == self.ui.button3:
+            nappi = 3
+        elif self.sender() == self.ui.button4:
+            nappi = 4
+        else:
+            return
+        
+        if nappi == self.oikea_vastaus:
+            print("Oikein!")
+            self.pisteet += 1
+            # tai self.pisteet = self.pisteet + 1
+        
+        self.indeksi += 1
+        if self.indeksi >= len(KYSYMYKSET_JA_VASTAUKSET):
+            laatikko = QMessageBox(self)
+            laatikko.setText(f"Peli päättyi! Sait {self.pisteet} pistettä.")
+            laatikko.exec()
+            self.indeksi = 0
+            self.pisteet = 0
+        self.vaihda_kysymys_ja_vastaukset(self.indeksi)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
